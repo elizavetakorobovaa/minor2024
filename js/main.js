e537da998dfb59f39dd94dfdc151edba7dfc491e
elizavetakorobovaa <elizavetakorobovaa@gmail.com>
2024-11-06 12:03:46 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 let translated = true;
 function changeLanguage() {
   if (translated) {
-    document.getElementById("name").innerText = "Ivan";
-    document.getElementById("surname").innerText = "Ivanov";
-    document.getElementById("birthyear").innerText = "1970";
+    document.getElementById("name").innerText = "Elizaveta";
+    document.getElementById("surname").innerText = "Korobova";
+    document.getElementById("birthyear").innerText = "2004";
     translated = false;
   } else {
-    document.getElementById("name").innerText = "Иван";
-    document.getElementById("surname").innerText = "Иванов";
-    document.getElementById("birthyear").innerText = "1970";
+    document.getElementById("name").innerText = "Елизавета";
+    document.getElementById("surname").innerText = "Коробова";
+    document.getElementById("birthyear").innerText = "2004";
     translated = true;
   }
 }
