5dc1f509b8677461acf31b8108527f856b131c60
elizavetakorobovaa <elizavetakorobovaa@gmail.com>
2024-11-10 23:04:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 let translated = true;
 function changeLanguage() {
   if (translated) {
-    document.getElementById("name").innerText = "Elizaveta";
+    console.log("en")
     document.getElementById("surname").innerText = "Korobova";
+    document.getElementById("firstname").innerText = "Elizaveta";
+    document.getElementById("paternalname").innerText = "Olegovna";
     document.getElementById("birthyear").innerText = "2004";
+    document.getElementById("gender").innerText = "fem";
+    document.getElementById("cityofbirth").innerText = "Moscow";
     translated = false;
   } else {
-    document.getElementById("name").innerText = "Елизавета";
+    console.log("ru")
     document.getElementById("surname").innerText = "Коробова";
+    document.getElementById("firstname").innerText = "Елизавета";
+    document.getElementById("paternalname").innerText = "Олеговна";
     document.getElementById("birthyear").innerText = "2004";
+    document.getElementById("gender").innerText = "жен.";
+    document.getElementById("cityofbirth").innerText = "Москва";
     translated = true;
   }
 }
